f62d7e3988e3c9fb410acf8c3fb8b6e72580b4d2
pongdang <pongpong0121@kakao.com>
2022-10-14 00:32:46 +0900

    chore: 코드 간격 추가

--- src/api/signIn.ts
+++ src/api/signIn.ts
@@ -2,5 +2,6 @@ import { post } from "./http";
 
 export const signIn = async ({ email, password }: { email: string; password: string }) => {
   const { data } = await post("/auth/signin", { email, password });
+
   localStorage.setItem("access_token", data.access_token);
 };
