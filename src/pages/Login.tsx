3a204ade051de8e0eeacd2fb4dfbb45593a2ea2f
pongdang <pongpong0121@kakao.com>
2022-10-13 15:49:29 +0900

    feat: useFormField hook 적용

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,76 +1,55 @@
-import { useEffect, useState } from "react";
+import useFormField from "../hooks/useFormField";
 
 export default function Login() {
-  const [userEmail, setUserEmail] = useState<string>("");
-  const [userPassword, setUserPassword] = useState<string>("");
-  const [validate, setValidate] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>("");
+  const {
+    value: userEmail,
+    onChange: onChangeUserEmail,
+    errorMessage: emailErrorMessage,
+  } = useFormField([
+    { valid: (value) => value !== "", message: "이메일을 입력해주세요" },
+    { valid: (value) => value.includes("@"), message: "이메일 형식이 올바르지 않습니다" },
+  ]);
 
-  const validateForm = () => {
-    if (userEmail === "") {
-      setErrorMessage("이메일을 입력해주세요");
-      return setValidate(false);
-    }
+  const {
+    value: userPassword,
+    onChange: onChangeUserPassword,
+    errorMessage: passwordErrorMessage,
+  } = useFormField([
+    { valid: (value) => value !== "", message: "비밀번호를 입력해주세요" },
+    { valid: (value) => value.length >= 8, message: "비밀번호는 8자 이상이어야 합니다" },
+  ]);
 
-    if (userPassword === "") {
-      setErrorMessage("비밀번호를 입력해주세요");
-      return setValidate(false);
-    }
-
-    if (!userEmail.includes("@")) {
-      setErrorMessage("이메일 형식이 올바르지 않습니다");
-      return setValidate(false);
-    }
-
-    if (userPassword.length < 8) {
-      setErrorMessage("비밀번호는 8자 이상이어야 합니다");
-      return setValidate(false);
-    }
-
-    setErrorMessage("");
-    return setValidate(true);
-  };
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserPassword(e.target.value);
-  };
+  console.log(emailErrorMessage, passwordErrorMessage);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (validate) {
+    if (emailErrorMessage == null && passwordErrorMessage == null) {
+      console.log("로그인");
       console.log(userEmail, userPassword);
     }
   };
 
-  useEffect(() => {
-    validateForm();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [handleSubmit]);
-
   return (
     <>
       <h1>Login Page</h1>
-      <form onSubmit={(e) => handleSubmit(e)}>
+      <form onSubmit={handleSubmit}>
         <dl>
           <dt>
             <label htmlFor='user-email'>이메일</label>
           </dt>
           <dd>
-            <input type='email' id='user-email' name='user-email' onChange={handleEmailChange} />
+            <input type='email' id='user-email' name='user-email' onChange={onChangeUserEmail} />
+            <p>{emailErrorMessage}</p>
           </dd>
           <dt>
             <label htmlFor='user-password'>비밀번호</label>
           </dt>
           <dd>
-            <input type='password' id='user-password' name='user-password' onChange={handlePasswordChange} />
+            <input type='password' id='user-password' name='user-password' onChange={onChangeUserPassword} />
+            <p>{passwordErrorMessage}</p>
           </dd>
         </dl>
-        <p>{errorMessage}</p>
         <button type='submit'>로그인</button>
       </form>
       <button type='button'>회원가입</button>
