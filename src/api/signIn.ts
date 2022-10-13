2615f3afce682cb9901565484c61560697f0cb37
pongdang <pongpong0121@kakao.com>
2022-10-14 00:29:08 +0900

    feat: 로컬스토리지 저장하는 코드 추가

--- src/api/signIn.ts
+++ src/api/signIn.ts
@@ -1,5 +1,6 @@
 import { post } from "./http";
 
-export const signIn = ({ email, password }: { email: string; password: string }) => {
-  return post("/auth/signin", { email, password });
+export const signIn = async ({ email, password }: { email: string; password: string }) => {
+  const { data } = await post("/auth/signin", { email, password });
+  localStorage.setItem("access_token", data.access_token);
 };
