b87db30c2669c781909d09b13f8d96ab16444b2a
pongdang <pongpong0121@kakao.com>
2022-10-16 03:24:08 +0900

    refactor: router는 hoc로 개선

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,16 +4,41 @@ import { Join } from "./pages/Join";
 import { Login } from "./pages/Login";
 import TodoList from "./pages/TodoList";
 
-export function Router() {
-  const { isLogin } = useTokenContext();
+const GuestRoutes = ["Login", "Join"];
+const MemberRoutes = ["TodoList"];
 
+export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={isLogin ? <Navigate replace to='/todo' /> : <Login />} />
-        <Route path='/signup' element={isLogin ? <Navigate replace to='/todo' /> : <Join />} />
-        <Route path='/todo' element={isLogin ? <TodoList /> : <Navigate replace to='/' />} />
+        <Route path='/' element={withAuthGuard(<Login />)} />
+        <Route path='/signup' element={withAuthGuard(<Join />)} />
+        <Route path='/todo' element={withAuthGuard(<TodoList />)} />
       </Routes>
     </BrowserRouter>
   );
+}
+
+// TODO: components 디렉토리로 이동하기
+function Redirect({ to }: { to: string }) {
+  return <Navigate replace to={to} />;
+}
+
+// TODO: hocs 디렉토리로 이동하기
+function withAuthGuard(Component: JSX.Element) {
+  return (() => {
+    const { isLogin } = useTokenContext();
+
+    const { name } = Component.type;
+
+    if (!isLogin && MemberRoutes.includes(name)) {
+      return <Redirect to='/' />;
+    }
+
+    if (isLogin && GuestRoutes.includes(name)) {
+      return <Redirect to='/Todo' />;
+    }
+
+    return Component;
+  })();
 }
